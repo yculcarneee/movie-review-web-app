4890dac5203d865d38b2dd4381c48470267e6867
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-15 15:45:48 -0800

    Show title and movie card component in front end

--- movie_review_app_backend/movie_review_app_frontend/src/App.js
+++ movie_review_app_backend/movie_review_app_frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieCard from './components/movie_card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Movie Review App </h1>
+      <MovieCard/>
     </div>
   );
 }
