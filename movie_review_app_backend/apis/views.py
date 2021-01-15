2b9a08231c70bd3a814b1ab802b44ad33b2e178e
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-15 14:05:10 -0800

    Change movies/ API endpoint to return only relevant data

--- movie_review_app_backend/apis/views.py
+++ movie_review_app_backend/apis/views.py
@@ -6,13 +6,32 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 import requests
+import json
 
 # Create your views here.
 @api_view(["GET"])
 def main(request):
-    endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key='+settings.TMDB_API_KEY+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+    endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key='+settings.TMDB_API_KEY+'&language=en-US&sort_by=popularity.desc'
     response = requests.get(endpoint)
 
-    movies = response.json()
+    data = response.json()
 
-    return Response(status=status.HTTP_200_OK, data={"movies": movies})
+    movies = {}
+
+    movies['page'] = data['page']
+    movies['total_pages'] = data['total_pages']
+    movies['total_results'] = data['total_results']
+    movies['results'] = []
+
+    for obj in data['results']:
+        resultObj = {}
+
+        resultObj['id'] = obj['id']
+        resultObj['title'] = obj['title']
+        resultObj['overview'] = obj['overview']
+        resultObj['release_date'] = obj['release_date']
+        resultObj['poster'] = 'https://image.tmdb.org/t/p/w500' + obj['poster_path']
+
+        movies['results'].append(resultObj)
+
+    return Response(status=status.HTTP_200_OK, data=movies)
