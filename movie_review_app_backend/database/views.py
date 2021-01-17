1981f0d0a96542fee2f168186c37e21fe4766493
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-16 23:21:27 -0800

    Create endpoint to add a movie into WatchedMovieDatabase

--- movie_review_app_backend/database/views.py
+++ movie_review_app_backend/database/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpRequest 
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import WatchedMoviesDatabase
+
+import json
 
 # Create your views here.
+@api_view(["POST"])
+def addToWatchedList(request: HttpRequest):
+    
+    data = json.loads(request.body)
+
+    databaseEntry = WatchedMoviesDatabase(movieId = data['movieId'], movieName = data['movieName'])
+    databaseEntry.save()
+
+    return Response(status=status.HTTP_200_OK)
+
