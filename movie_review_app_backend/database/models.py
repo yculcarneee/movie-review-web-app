6c282397dc4d8d0879c72e2314f9223a06594cb1
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-16 22:37:56 -0800

    Create WatchedMoviesDatabase model for storing watched movies details

--- movie_review_app_backend/database/models.py
+++ movie_review_app_backend/database/models.py
@@ -1,5 +1,9 @@
+# models.py
 from django.db import models
 
 # Create your models here.
+class WatchedMoviesDatabase(models.Model):
+    movieId = models.IntegerField(primary_key=True)
+    movieName = models.TextField()
 
     
