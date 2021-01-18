5e1b612849bd08f88954598733b1bb1c770d77d3
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-18 14:10:47 -0800

    Write test for getMovieDetails/ endpoint using mocking

--- movie_review_app_backend/apis/tests/testViews.py
+++ movie_review_app_backend/apis/tests/testViews.py
@@ -118,3 +118,44 @@ class TestViews(TestCase):
         endpoint = 'https://api.themoviedb.org/3/discover/movie?api_key='+settings.TMDB_API_KEY+'&language=en-US&sort_by=popularity.desc&page='+str(page)
 
         mock_get.assert_called_once_with(endpoint)
+
+    @patch('apis.views.requests.get')
+    def testGetMovieDetailsView(self, mock_get):
+        mock_response = Mock()
+        
+        expected_response = {    
+            'adult': False, 
+            'backdrop_path': '/srYya1ZlI97Au4jUYAktDe3avyA.jpg', 
+            'genre_ids': [14, 28, 12], 
+            'id': 1, 
+            'original_language': 'en', 
+            'original_title': 'Dummy Text', 
+            'overview': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', 
+            'popularity': 4160.738, 
+            'poster_path': '/kqjL17yufvn9OVLyXYpvtyrFfak.jpg', 
+            'release_date': '2020-12-16', 
+            'title': 'Dummy Movie Name', 
+            'video': False, 
+            'vote_average': 7.1, 
+            'vote_count': 2781
+        }
+
+        mock_response.json.return_value = expected_response
+        mock_response.status_code = 200
+
+        mock_get.return_value = mock_response
+
+        response = self.client.get(reverse('getMovieDetails', args=['1']))
+        results = response.json()
+
+        self.assertEquals(results, {
+            'movieName': 'Dummy Movie Name',
+            'movieOverview': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            'movieReleaseDate': '2020-12-16',
+            'moviePoster': 'https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg'
+        })
+
+        movieId = 1
+        endpoint = 'https://api.themoviedb.org/3/movie/' + str(movieId) + '?api_key=' + settings.TMDB_API_KEY + '&language=en-US'
+
+        mock_get.assert_called_once_with(endpoint)
