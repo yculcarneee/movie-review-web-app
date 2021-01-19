bfc1b1d4dea633230572158d943c413ec0f9f26c
Yash Kulkarni <54493779+yculcarneee@users.noreply.github.com>
2021-01-19 10:20:50 -0800

    Add React tests for Main Page component

---
+++ movie_review_app_backend/movie_review_app_frontend/src/__tests__/MainPageTests.js
@@ -0,0 +1,111 @@
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import Main from '../containers/main'
+
+import { enableFetchMocks } from 'jest-fetch-mock'
+
+enableFetchMocks()
+
+describe('Main Page', () => {
+
+    beforeEach(() => {
+        fetch.resetMocks()
+    })
+
+    const moviesInCurPageResponse = {
+        "page": 1,
+        "total_pages": 500,
+        "total_results": 10000,
+        "results": [
+            {
+                "id": 716703,
+                "title": "Alpha Beta",
+                "overview": "Lorem Ipsum Dolor Sit Amet",
+                "release_date": "2020-12-17",
+                "poster": "https://image.tmdb.org/t/p/w500/zjGrlEuENSjRZGWc8N10zq3Jjkq1.jpg"
+            },
+            {
+                "id": 716704,
+                "title": "Gamma Theta",
+                "overview": "Lorem Ipsum Dolor Sit Amet Dos",
+                "release_date": "2021-12-17",
+                "poster": "https://image.tmdb.org/t/p/w500/zjGrlEuENSjRZGWc8N10zq3Jjkq2.jpg"
+            },
+        ]
+    }
+
+    const checkPageInWatchedListResponse = {
+        '716703': true,
+        '716704': false
+    }
+
+    const getCurrentPageMovieRatingsResponse = {
+        '716703': 4,
+        '716704': 5
+    }
+
+    test('Renders Navbar component', async() => {
+
+        fetch.mockResponseOnce(JSON.stringify(moviesInCurPageResponse))
+            .mockResponseOnce(JSON.stringify(checkPageInWatchedListResponse))
+            .mockResponseOnce(JSON.stringify(getCurrentPageMovieRatingsResponse))
+
+        render(
+            <BrowserRouter>
+                <Main/>
+            </BrowserRouter>
+        )
+
+        await waitForElementToBeRemoved(screen.getByText('Loading...'))
+
+        expect(screen.getByRole('banner')).toBeInTheDocument()
+        expect(screen.getByText('Movie Review App')).toBeInTheDocument()
+        expect(screen.getByText('Overview')).toBeInTheDocument()
+    })
+
+    test('Renders Movie Cards fully', async() => {
+
+        fetch.mockResponseOnce(JSON.stringify(moviesInCurPageResponse))
+            .mockResponseOnce(JSON.stringify(checkPageInWatchedListResponse))
+            .mockResponseOnce(JSON.stringify(getCurrentPageMovieRatingsResponse))
+
+        render(
+            <BrowserRouter>
+                <Main/>
+            </BrowserRouter>
+        )
+
+        await waitForElementToBeRemoved(screen.getByText('Loading...'))
+
+        expect(screen.getByText(moviesInCurPageResponse.results[0].title)).toBeInTheDocument()
+        expect(screen.getByText(moviesInCurPageResponse.results[1].title)).toBeInTheDocument()
+
+        expect(screen.getByText(moviesInCurPageResponse.results[0].overview)).toBeInTheDocument()
+        expect(screen.getByText(moviesInCurPageResponse.results[0].overview)).toBeInTheDocument()
+
+        expect(screen.getByText(moviesInCurPageResponse.results[0].release_date)).toBeInTheDocument()
+        expect(screen.getByText(moviesInCurPageResponse.results[0].release_date)).toBeInTheDocument()
+
+        expect(screen.getAllByRole('img')[0].src).toBe(moviesInCurPageResponse.results[0].poster)
+        expect(screen.getAllByRole('img')[1].src).toBe(moviesInCurPageResponse.results[1].poster)
+
+        expect(screen.queryAllByRole('radio')).not.toBeNull() 
+    })
+
+    test('Renders Pagniation component', async() => {
+
+        fetch.mockResponseOnce(JSON.stringify(moviesInCurPageResponse))
+            .mockResponseOnce(JSON.stringify(checkPageInWatchedListResponse))
+            .mockResponseOnce(JSON.stringify(getCurrentPageMovieRatingsResponse))
+
+        render(
+            <BrowserRouter>
+                <Main/>
+            </BrowserRouter>
+        )
+
+        await waitForElementToBeRemoved(screen.getByText('Loading...'))
+
+        expect(screen.getByRole('navigation')).toBeInTheDocument()
+    })
+})
